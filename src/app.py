9b14376580a11d89cf64eb375ba524ad551610f5
zahra ehghaghi <z.ehghaghi@gmail.com>
2026-05-26 06:59:22 -0400

    add ci

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'hostname': socket.gethostname(),
         'time':  datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
-        'message': "Hallo man. Du machst supper"
+        'message': "Hallo man. Du machst toll "
     })
 
 
